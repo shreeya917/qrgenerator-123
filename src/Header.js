a4a1c38b91c4c3e9cf16c6b9e32eb27adfba988d
Shreeya Regmi <shreeyaregmi6@gmail.com>
2021-04-06 11:52:35 +0545

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+    <header className="Header">
+        <span>QRCODE GENERATOR</span>
+    </header>
+);
+export default Header;
