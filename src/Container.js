37aacd87b74fe5e84b7e052aab3eb1a4c85d4b96
Shreeya Regmi <shreeyaregmi6@gmail.com>
2021-04-06 11:50:32 +0545

    Add files via upload

---
+++ src/Container.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import App from './App';
+import Header from './Header';
+import Footer from './Footer';
+
+
+const Container = () => (
+    <div className="Container">
+
+        < Header />
+        <App />
+        <Footer />
+
+
+    </div>
+
+)
+
+export default Container;
